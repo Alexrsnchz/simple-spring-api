6b42eb1ea1b2d143c179da6624baa985b3e05ea8
Alexrsnchz <alexrsnchz@gmail.com>
2025-03-18 18:58:49 +0100

    build: Project start

---
+++ src/main/java/com/alexrsnchz/simplespringapi/SimplespringapiApplication.java
@@ -0,0 +1,13 @@
+package com.alexrsnchz.simplespringapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimplespringapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SimplespringapiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/alexrsnchz/simplespringapi/SimplespringapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alexrsnchz.simplespringapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimplespringapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
